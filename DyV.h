33c7425099cf6d1ca1d13671958320635cbb45ea
jorter <jortter@etsinf.upv.es>
2024-11-14 18:00:39 +0100

    Terminado DyV.h y el test

--- DyV.h
+++ DyV.h
@@ -46,3 +46,19 @@ int BusquedaBinaria(T x, std::vector<T>& v, int ini, int fin){
 		return BusquedaBinaria(x, v, medio +1, fin);
 	}
 }
+
+template <typename T>
+int BusquedaBinaria_INV(T x, std::vector<T>& v, int ini, int fin){
+	if(ini > fin){
+		return -1;
+	}
+	int medio = (ini + fin) / 2;
+	if(v[medio] == x){
+		return medio;
+	}
+	else if(v[medio] > x){
+		return BusquedaBinaria_INV(x, v, medio +1, fin);
+	}else{
+		return BusquedaBinaria_INV(x, v, ini, medio -1);
+	}
+}
